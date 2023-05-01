1f2a6016602d3f275fcb3078206ab527bd540fe3
Jessica Kuijer <jessicakuijer@me.com>
2023-05-01 15:08:32 +0200

    🐛 fix(cache.yaml): add cache pool configuration for app.api_cache ✨ feat(services.yaml): add OpenAiService to services and inject cache.app 🚀 perf(OpenAiService.php): add caching to getArtistSuggestions method The cache pool configuration for app.api_cache has been added to cache.yaml. The OpenAiService has been added to services.yaml and the cache.app service has been injected into the OpenAiService constructor. The getArtistSuggestions method in OpenAiService has been updated to use caching to store and retrieve the results of the API call. This improves performance by reducing the number of API calls made.

--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -7,14 +7,18 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Tectalic\OpenAi\Manager;
 use Tectalic\OpenAi\Models\ChatCompletions\CreateRequest;
 use Symfony\Component\HttpClient\Psr18Client;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class OpenAiService
 {
     private ParameterBagInterface $parameterBag;
+    private $cache;
 
-    public function __construct(ParameterBagInterface $parameterBag)
+    public function __construct(ParameterBagInterface $parameterBag, CacheInterface $apiCache)
     {
         $this->parameterBag = $parameterBag;
+        $this->cache = $apiCache;
     }
 
     private function formatResponse(string $response): string
@@ -49,41 +53,55 @@ class OpenAiService
 
     public function getArtistSuggestions(string $query): array
     {
-        $openAiKey = $this->parameterBag->get('OPENAI_API_KEY');
-        $httpClient = new Psr18Client();
-        $openAiClient = Manager::build($httpClient, new Authentication($openAiKey));
-        
-        $prompt = "Tu es mélomane. En priorité, élabore une liste de dix artistes émergents et similaires à l'artiste recherché en donnant un lien pour acheter leur musique sur bandcamp et suggère d'autres noms de plateformes sans url. Tu peux justifier tes choix d'artistes similaires émergents si tu le souhaites. Enfin, si tu n'es pas en mesure d'avoir un url de bandcamp valide, réponds par une phrase qui induit une possibilité d'url invalide et suggère uniquement des noms de plateforme sans url et l'url direct de https://bandcamp.com/ pour faire une recherche manuelle. l'artiste auquel les artistes émergents doivent être similaires est : $query: \n\n";
+        // Utilisez le pool de cache pour stocker et récupérer les résultats de l'API
+        return $this->cache->get(md5($query), function (ItemInterface $item) use ($query) {
+            $item->expiresAfter(3600); // Durée de vie du cache en secondes (1 heure dans cet exemple)
+    
+            // Appelez l'API et récupérez les données
+            $apiData = $this->callOpenAiApi($query);
+    
+            return $apiData;
+        });
+    }
 
-        $request = $openAiClient->chatCompletions()->create(
+    private function callOpenAiApi(string $query): array
+    {
+    $openAiKey = $this->parameterBag->get('OPENAI_API_KEY');
+    $httpClient = new Psr18Client();
+    $openAiClient = Manager::build($httpClient, new Authentication($openAiKey));
+
+    $prompt = "Tu es mélomane. En priorité, élabore une liste de dix artistes émergents et similaires à l'artiste recherché en donnant un lien pour acheter leur musique sur bandcamp et suggère d'autres noms de plateformes sans url. Tu peux justifier tes choix d'artistes similaires émergents si tu le souhaites. Enfin, si tu n'es pas en mesure d'avoir un url de bandcamp valide, réponds par une phrase qui induit une possibilité d'url invalide et suggère uniquement des noms de plateforme sans url et l'url direct de https://bandcamp.com/ pour faire une recherche manuelle. l'artiste auquel les artistes émergents doivent être similaires est : $query: \n\n";
+
+    $request = $openAiClient->chatCompletions()->create(
         new CreateRequest([
             'model' => 'gpt-4',
             'messages' => [
                 ['role' => 'system', 'content' => 'You are a helpful assistant.'],
                 ['role' => 'user', 'content' => $prompt],
-                ],
-                'temperature' => 0.7,
-                'max_tokens' => 1000,
-                'frequency_penalty' => 0.3,
-                'presence_penalty' => 0.5,
-                'n' => 1,
-                'stop' => null,
-                'best_of' => 1
+            ],
+            'temperature' => 0.7,
+            'max_tokens' => 1000,
+            'frequency_penalty' => 0.3,
+            'presence_penalty' => 0.5,
+            'n' => 1,
+            'stop' => null,
+            'best_of' => 1
         ])
-        )->toModel();
+    )->toModel();
 
-        if (
-            isset($request->choices) &&
-            isset($request->choices[0]) &&
-            isset($request->choices[0]->message) &&
-            isset($request->choices[0]->message->content)
-        ) {
-            $response = $request->choices[0]->message->content;
-            $response = $this->formatResponse($response);
-        } else {
-            $response = "Une erreur est survenue dans la réponse d'OpenAI.";
-        }
-    
-        return ['suggestion' => $response];
+    if (
+        isset($request->choices) &&
+        isset($request->choices[0]) &&
+        isset($request->choices[0]->message) &&
+        isset($request->choices[0]->message->content)
+    ) {
+        $response = $request->choices[0]->message->content;
+        $response = $this->formatResponse($response);
+    } else {
+        $response = "Une erreur est survenue dans la réponse d'OpenAI.";
     }
+
+    return ['suggestion' => $response];
+    }
+
 }
