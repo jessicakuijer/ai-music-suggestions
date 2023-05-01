502a6d91d750021ff7d2fea862900852b866a3b6
Jessica Kuijer <jessicakuijer@me.com>
2023-05-01 13:30:25 +0200

    🔨 refactor(OpenAiService.php): refactor formatResponse method ✨ feat(OpenAiService.php): add numbered list of artist suggestions The formatResponse method has been refactored to improve readability and maintainability. The method now uses consistent indentation and spacing. Additionally, the method now includes a numbered list of artist suggestions. This improves the user experience by providing a clear and concise list of artist suggestions. The getArtistSuggestions method prompt has also been updated to be more clear and concise.

--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -18,7 +18,7 @@ class OpenAiService
     }
 
     private function formatResponse(string $response): string
-{
+    {
     // Transforme les URL en liens cliquables
     $response = preg_replace_callback(
         '~\b(?:https?://|www\.)\S+\b~',
@@ -32,13 +32,19 @@ class OpenAiService
             return '<a href="' . $urlWithPrefix . '" target="_blank">' . $url . '</a>';
         },
         $response
-    );
+    ); 
 
     // Ajoute un retour à la ligne après chaque point qui suit un mot, en ignorant les URL
-    $response = preg_replace('/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.(?!(com|org|net|gov|edu|io|co|us)\b))\s+/', "<br>", $response);
+    $response = preg_replace('/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.(?!(com|org|net|gov|edu|io|co|us)\b))\s+/', "<br>", $response); 
+
+    // Transforme la liste d'artistes en liste numérotée sans les numéros répétés
+    $response = preg_replace('/(\d+)\.\s*(.*\s*-\s*<a\s*href=\".*\"\s*target=\"_blank\">.*<\/a>\s*)/', '<li>$2</li>', $response);
+
+    // Ajoute les balises <ol> et </ol> autour de la liste numérotée
+    $response = preg_replace('/(<li>.*<\/li>)/s', '<ol>$1</ol>', $response);
 
     return $response;
-}
+    }
 
 
     public function getArtistSuggestions(string $query): array
@@ -47,7 +53,7 @@ class OpenAiService
         $httpClient = new Psr18Client();
         $openAiClient = Manager::build($httpClient, new Authentication($openAiKey));
         
-        $prompt = "Donne moi une dizaine d'artistes émergents et similaires à cet artiste, donne un lien pour acheter la musique de cet artiste sur bandcamp et suggère d'autres noms de plateformes sans url. Si tu n'es pas en mesure d'avoir un url de bandcamp pour l'artiste donné, réponds par une phrase qui induit une possibilité d'url invalide et suggère uniquement des noms de plateforme sans url et l'url direct de https://bandcamp.com/ pour faire une recherche manuelle: $query: \n\n";
+        $prompt = "Tu es mélomane. En priorité, élabore une liste de dix artistes émergents et similaires à l'artiste recherché en donnant un lien pour acheter leur musique sur bandcamp et suggère d'autres noms de plateformes sans url. Tu peux justifier tes choix d'artistes similaires émergents si tu le souhaites. Enfin, si tu n'es pas en mesure d'avoir un url de bandcamp valide, réponds par une phrase qui induit une possibilité d'url invalide et suggère uniquement des noms de plateforme sans url et l'url direct de https://bandcamp.com/ pour faire une recherche manuelle. l'artiste auquel les artistes émergents doivent être similaires est : $query: \n\n";
 
         $request = $openAiClient->chatCompletions()->create(
         new CreateRequest([
