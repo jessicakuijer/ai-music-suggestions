e9b6d172298e75de4c7c4a514245c0f3e41ecbd3
Jessica Kuijer <jessicakuijer@me.com>
2023-05-02 18:58:16 +0200

    debugbundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    /* Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true], */
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
