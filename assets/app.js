f8b6a1ba8b91f8507c139cc0c334690f77434b1d
Jessica Kuijer <jessicakuijer@me.com>
2023-05-04 12:22:54 +0200

    ✨ fonctionnalité(app.js) : ajouter un loader lors de la recherche d'un artiste et un message de chargement ✨ fonctionnalité(index.html.twig) : ajouter une div pour afficher le loader et le message de chargement Lorsqu'un utilisateur recherche un artiste, un loader est maintenant affiché avec un message de chargement pour indiquer que les résultats sont en cours de chargement. La div pour afficher le loader et le message de chargement a été ajoutée dans le fichier index.html.twig. Cela améliore l'expérience utilisateur en fournissant une indication visuelle que quelque chose se passe pendant la recherche.

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,42 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+// ajout d'une fonctionnalité pour afficher le loader lors de la recherche d'un artiste et message de chargement
+
+document.addEventListener('DOMContentLoaded', function () {
+    const submitButton = document.getElementById('artist_form_submit');
+    const loadingMessage = document.getElementById('loading-message');
+    const loadingText = document.getElementById('loading-text');
+
+    if (submitButton) {
+        submitButton.addEventListener('click', function () {
+            const buttonText = [
+                "Résultats en cours de chargement...",
+                "En cours d'analyse...",
+                "Encore un peu de patience...",
+            ];
+            let index = 0;
+
+            const updateButtonText = () => {
+                loadingText.textContent = buttonText[index % buttonText.length];
+                index++;
+            };
+
+            updateButtonText();
+            loadingMessage.style.display = 'inline-block'; // Affiche la div de chargement
+            const intervalId = setInterval(updateButtonText, 2000); // Change le texte toutes les 2 secondes
+
+            // Arrête de mettre à jour le texte lorsque les résultats sont chargés
+            document.body.addEventListener(
+                'htmx:afterSwap',
+                function () {
+                    clearInterval(intervalId);
+                    loadingMessage.style.display = 'none'; // Cache la div de chargement
+                },
+                { once: true }
+            );
+        });
+    }
+});
+
