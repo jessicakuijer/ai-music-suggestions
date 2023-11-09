f0c6cc1dac6cd002d261d89dce3a7d2d051d2c96
Jessica Kuijer <jessicakuijer@me.com>
2023-11-09 15:54:13 +0100

    🐛 fix(OpenAiService.php): update OpenAi model from 'gpt-3.5-turbo' to 'gpt-4-turbo' to use the latest version 📝 chore(OpenAiService.php): improve user messages for generating short biography and finding similar emerging artists

--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -66,8 +66,8 @@ class OpenAiService
     
     $messages = [
         ['role' => 'system', 'content' => 'You are a helpful assistant.'],
-        ['role' => 'user', 'content' => "Premièrement, rédige une très courte biographie d'un paragraphe de $query."],
-        ['role' => 'user', 'content' => "Je recherche ensuite cinq artistes émergents similaires à $query."],
+        ['role' => 'user', 'content' => "Premièrement, rédige une très courte biographie incluant ses dernières actualités musicales d'un paragraphe de $query."],
+        ['role' => 'user', 'content' => "Je recherche ensuite cinq artistes émergents et récents similaires à $query."],
         ['role' => 'user', 'content' => "Fournissez un lien pour acheter leur musique sur Bandcamp pour chaque artiste suggéré."],
         ['role' => 'user', 'content' => "Si un lien Bandcamp valide n'est pas disponible, mentionnez les noms des plateformes et l'URL directe de https://bandcamp.com/ pour effectuer une recherche manuelle."],
         ['role' => 'user', 'content' => "Justifiez vos choix d'artistes similaires émergents à la fin de la liste de manière générale."]
@@ -75,7 +75,7 @@ class OpenAiService
 
     $request = $openAiClient->chatCompletions()->create(
         new CreateRequest([
-            'model' => 'gpt-3.5-turbo',
+            'model' => 'gpt-4-1106-preview',
             'messages' => $messages,
             'temperature' => 0.5, // Réduisez la valeur de la température
             'max_tokens' => 800, // Réduisez le nombre maximal de tokens
